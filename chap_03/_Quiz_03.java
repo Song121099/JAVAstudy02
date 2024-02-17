98f7ef95c7eed6e28f10e203b592384c04d97b8a
hardwoong <158552165+hardwoong@users.noreply.github.com>
2024-02-17 14:16:15 +0900

    Delete chap_03 directory

--- chap_03/_Quiz_03.java
+++
@@ -1,9 +0,0 @@
-package chap_03;
-
-public class _Quiz_03 {
-    public static void main(String[] args) {
-        String s = "010430-1234567";
-        System.out.println(s.substring(0, 8));
-        System.out.println(s.substring(0, s.indexOf("-")+2));
-    }
-}
